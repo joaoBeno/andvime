6e2a157e1c59b7ca1606c1f9fb36447e890963a0
João Beno <joao@beno.net.br>
2015-09-04 17:41:46 -0300

    Alerta de carregando e pesquisa com animação

--- app/src/main/java/br/net/beno/andvime/ListaVideosActivity.java
+++ app/src/main/java/br/net/beno/andvime/ListaVideosActivity.java
@@ -1,14 +1,26 @@
 package br.net.beno.andvime;
 
+import android.animation.LayoutTransition;
+import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDialog;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -23,6 +35,9 @@ public class ListaVideosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_videos);
 
+        CarregandoDialogFragment cdf = new CarregandoDialogFragment();
+        cdf.show(getSupportFragmentManager(),"tag");
+
         RecyclerView rv = (RecyclerView)findViewById(R.id.view_recicla);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
@@ -35,6 +50,25 @@ public class ListaVideosActivity extends AppCompatActivity {
         RVAdapter adapter = new RVAdapter(videos);
         rv.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Inflate menu to add items to action bar if it is present.
+        inflater.inflate(R.menu.menu_main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        LinearLayout searchBar = (LinearLayout) searchView.findViewById(R.id.search_bar);
+        searchBar.setLayoutTransition(new LayoutTransition());
+
+        return true;
     }
 
     public class RVAdapter extends RecyclerView.Adapter<RVAdapter.VideoViewHolder>{
@@ -84,4 +118,27 @@ public class ListaVideosActivity extends AppCompatActivity {
             super.onAttachedToRecyclerView(recyclerView);
         }
     }
+
+    public static class CarregandoDialogFragment extends AppCompatDialogFragment {
+        @Override
+        public AppCompatDialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Por favor aguarde...").setTitle("Carregando vídeos");
+                    /*.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });*/
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
+
 }
